d39165769a020ade9c4b715815834c3ae1822584
s.pourabbas <sara.poorabas@gmail.com>
2025-05-16 19:39:55 +0330

    change classname in GameBoard page

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -14,28 +14,19 @@ const GameBoard = ({ rows, cols, oddCell, baseColor, oddColor, onSelect }: GameB
   return (
     <div className="flex justify-center items-center w-full">
       <div
-        className="grid "
+        className="grid w-[600px] h-[400px] box-border [gap:2px]"
         style={{
-          width: "600px",
-          height: "400px",
-          display: "grid",
           gridTemplateColumns: `repeat(${cols}, 1fr)`,
           gridTemplateRows: `repeat(${rows}, 1fr)`,
-          gap: "2px",
-          // padding: "10px",
-          boxSizing: "border-box",
         }}
       >
         {Array.from({ length: totalCells }).map((_, index) => (
           <button
             key={index}
             onClick={() => onSelect(index)}
-            className="rounded"
+            className="rounded w-full h-full border border-[#fff]"
             style={{
               backgroundColor: index === oddCell ? oddColor : baseColor,
-              width: "100%",
-              height: "100%",
-              border: "1px solid #fff",
             }}
           />
         ))}
